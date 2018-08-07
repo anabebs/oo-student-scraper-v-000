0d1d87fc0947606f12320b0e19ea142900aa2737
Ana Bebic <anabebic.x@gmail.com>
2018-08-07 13:38:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,6 +42,21 @@ class Scraper
     end
     master_array
   end
+  
+  def self.scrape_index_page(index_url)
+    	    students_hash = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect do |student|
+      hash = {
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student-location").text,
+        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
+      }
+      students_hash << hash
+    end
+    students_hash
+  end	  end
+
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
