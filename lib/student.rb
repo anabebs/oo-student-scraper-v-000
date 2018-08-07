234dad0f985d31c64d167f86599d0856e4d916e9
Ana Bebic <anabebic.x@gmail.com>
2018-08-07 13:35:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,16 @@ class Student
    def initialize(student_hash)	  
     	    student_hash.each {|k,v| self.send(("#{k}="),v)}
     @@all << self
-  end	  
+   
   end
    def self.create_from_collection(students_array)	  def self.create_from_collection(students_array)
     	    students_array.each{|student| Student.new(student)}
   end	  
-  end
+ 
    def add_student_attributes(attributes_hash)	 
     	    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
   end	
-  end
+  
    def self.all	 
     	    @@all
   end	 
