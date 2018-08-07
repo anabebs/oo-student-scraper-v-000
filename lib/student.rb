bdc2d821819557d55e050b7f65b298f2338dcb6b
Ana Bebic <anabebic.x@gmail.com>
2018-08-07 13:31:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,26 +1,17 @@
-class Student
+@@all = []
+   def initialize(student_hash)	  def initialize(student_hash)
+    	    student_hash.each {|k,v| self.send(("#{k}="),v)}
+    @@all << self
+  end	  end
+   def self.create_from_collection(students_array)	  def self.create_from_collection(students_array)
+    	    students_array.each{|student| Student.new(student)}
+  end	  end
+   def add_student_attributes(attributes_hash)	  def add_student_attributes(attributes_hash)
+    	    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
+  end	  end
+   def self.all	  def self.all
+    	    @@all
+  end	  end
+end	end
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
-
-  @@all = []
-student_hash = {}
-  def initialize(student_hash)
- @@all << self
-end
-
-  def self.create_from_collection(students_hash)
-    html = File.read('../fixtures/student-site/index.html')
-  list = Nokogiri::HTML(html)
- 
-  projects = {}
-  end
-
-  def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    @@all
-  end
-end
 
